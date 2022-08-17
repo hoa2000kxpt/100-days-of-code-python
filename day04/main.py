c31edb21c35a332908e7ba962bf5c1dfeb0acc1e
dvhoa <hoa2000kxpt@gmail.com>
2022-08-17 13:40:12 +0700

    new updates

--- day04/main.py
+++ day04/main.py
@@ -1,6 +1,5 @@
 import random
 
-
 rock = '''
     _______
 ---'   ____)
@@ -28,47 +27,59 @@ scissors = '''
 ---.__(___)
 '''
 
-
+# Human Choice
 choice = (input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n0/1/2:"))
-computers_choice = random.randint(0,2)
 
+# Computer Choice
+computers_choice = random.randint(0, 2)
+
+# If the user doesn't type a digit, display the error!
 if not choice.isdigit():
-  print("You've entered an invalid value, try again and choose a number between 0-2.")
+    print("You've entered an invalid value, try again and choose a number between 0-2.")
+
+# If the user types a digit, continue the game
 else:
-  choice = int(choice)
-  if choice > 2:
-    print("You've entered an invalid number, try again and choose a number between 0-2.")
-  elif choice == 0 or choice == 1 or choice == 2:
-    if choice == 0:
-      print(f"You chose: Rock {rock}")
-    elif choice == 1:
-      print(f"You chose: Paper {paper}")
-    elif choice == 2:
-      print(f"You chose: Scissors {scissors}")
+    choice = int(choice)
 
-    if computers_choice == 0:
-      print(f"The computer chose: Rock {rock}")
-      if choice == 2:
-        print("You lose, Rock wins against scissors.")
-      elif choice == 0:
-        print("It's a draw!")
-      else:
-        print("You win!")
+    # If the user enter more than 2, display the error
+    if choice > 2:
+        print("You've entered an invalid number, try again and choose a number between 0-2.")
 
-    elif computers_choice == 1:
-      print(f"The computer chose: Paper {paper}")
-      if choice == 0:
-        print("You lose, Paper wins against rock.")
-      elif choice == 0:
-        print("It's a draw!")
-      else:
-        print("You win!")
+    # If the user enter properly, print their choice
+    elif choice == 0 or choice == 1 or choice == 2:
+        if choice == 0:
+            print(f"You chose: Rock {rock}")
+        elif choice == 1:
+            print(f"You chose: Paper {paper}")
+        elif choice == 2:
+            print(f"You chose: Scissors {scissors}")
 
-    elif computers_choice == 2:
-      print(f"The computer chose: Scissors {scissors}")
-      if choice == 1:
-        print("You lose, Scissors win against paper.")
-      elif choice == 2:
-        print("It's a draw!")
-      else:
-        print("You win!")
+        # If the computer chooses Rock
+        if computers_choice == 0:
+            print(f"The computer chose: Rock {rock}")
+            if choice == 2:
+                print("You lose, Rock wins against scissors.")  # Rock vs Scissors
+            elif choice == 0:
+                print("It's a draw!")  # Rock vs Rock
+            else:
+                print("You win!")  # Rock vs Paper
+
+        # If the computer chooses Paper
+        elif computers_choice == 1:
+            print(f"The computer chose: Paper {paper}")
+            if choice == 0:
+                print("You lose, Paper wins against rock.")  # Paper vs Rock
+            elif choice == 0:
+                print("It's a draw!")   # Paper vs Paper
+            else:
+                print("You win!")   # Paper vs Scissors
+
+        # If the computer chooses Scissors
+        elif computers_choice == 2:
+            print(f"The computer chose: Scissors {scissors}")
+            if choice == 1:
+                print("You lose, Scissors win against paper.")   # Scissors vs Paper
+            elif choice == 2:
+                print("It's a draw!")   # Scissors vs Scissors
+            else:
+                print("You win!")   # Scissors vs Rock
--- day06/README.md
+++ day06/README.md
@@ -5,3 +5,8 @@
 - While Loops
 ## Escaping the Maze
 ![day06](https://user-images.githubusercontent.com/98851253/154312745-8abc5397-27b7-4a1d-b29c-3a1527280868.gif)
+
+## Reference:
+1. https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+2. https://docs.python.org/3/library/functions.html
+3. 
