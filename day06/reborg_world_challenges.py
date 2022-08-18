3bf4d548c62b98eac07111c254b573a196bbfaff
dvhoa <hoa2000kxpt@gmail.com>
2022-08-18 09:37:12 +0700

    new updates

---
+++ day06/reborg_world_challenges.py
@@ -0,0 +1,81 @@
+# Home 1:
+move()
+move()
+
+# Home 2:
+move()
+move()
+
+# Home 3:
+move()
+move()
+turn_left()
+move()
+
+# Home 4:
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def three_steps():
+    move()
+    move()
+    move()
+
+three_steps()
+turn_left()
+three_steps()
+turn_right()
+move()
+turn_right()
+three_steps()
+turn_left()
+three_steps()
+turn_right()
+move()
+turn_right()
+three_steps()
+turn_left()
+three_steps()
+turn_right()
+move()
+turn_right()
+three_steps()
+turn_left()
+three_steps()
+
+# Around 1:
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def go_straight():
+    move()
+    move()
+    move()
+    move()
+    move()
+    move()
+    move()
+    move()
+    move()
+
+
+for i in range(4):
+    go_straight()
+    turn_left()
+
+
+
+
+
+
+
+
+
+
+
+
