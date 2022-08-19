fd52a4dd556badfa579f4f112ef31e5b02666699
dvhoa <hoa2000kxpt@gmail.com>
2022-08-19 15:03:43 +0700

    new updates

--- day07/main.py
+++ day07/main.py
@@ -1,3 +1,38 @@
+"""
+Hints:
+#Step 1: Picking a random words and checking answers
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess lowercase.
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word
+
+#Step 2: Replacing Blanks with Guesses
+#TODO-1 - Create an empty List called display. For each letter in the chosen_word, add "_" to 'display'.
+So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_"
+representing each letter to guess.
+#TODO-2 - Loop through each position in the chosen_word; If the letter at that position matches 'guess the reveal
+that letter in the display at that position. (e.g: If the user guessed "p" and the chosen word was "apple", then
+display should be ["_", "p", "p", "_", "_"])
+#TODO-3 - Print 'display' and you should see the guessed letter in the correct position and every other letter
+replace with "_".
+
+#Step 3: Checking if the Player has won
+#TODO-1 - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the
+letters in the chosen_word and 'display' has more blanks ("_"). Then you can tell the user they've won.
+
+#Step 4: Keeping Track of the Player's Lives
+#TODO-1 - Create a variable called 'lives' to keep track of the number of lives left. Set 'lives' to equal 6.
+#TODO-2 - If guess is not a letter in the chosen_word, then reduce 'lives' by 1. If the lives goes down to 0 then the
+game should stop and it should print "You lose".
+#TODO-3 - Print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+
+#Step 5: Improve the User Experience
+#TODO-1 - Update the word list to use the 'word_list' from hangman_words.py.
+#TODO-2 - Import the stages from hangman_art.py and make this error go away.
+#TODO-3 - Import the logo from hangman_art.py and print it at the start of the game
+#TODO-4 - If the user has entered a letter they've already guessed, print the letter and let them know.
+#TODO-5 - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
+"""
+
 from replit import clear
 import random
 from hangman_words import word_list
