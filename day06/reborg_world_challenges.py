0d5a4fc9c297367f92b2925669d95a7986b2e7e3
dvhoa <hoa2000kxpt@gmail.com>
2022-08-30 15:42:08 +0700

    new updates

--- day06/reborg_world_challenges.py
+++ day06/reborg_world_challenges.py
@@ -273,6 +273,36 @@ while front_is_clear():
         put()
         move()
 
+"""
+Challenge 14: Harvest 1
+"""
+while front_is_clear():
+    move()
+
+    # Find the object at the both sides
+    if object_here():
+        turn_left()
+        turn_left()
+        take()
+        move()
+        put()
+        move()
+
+        # Find the object at the middle side
+        if object_here():
+            turn_left()
+            turn_left()
+            take()
+            move()
+            done()
+
+    # If the robot faces the wall, move back and put the object
+    if wall_in_front():
+        turn_left()
+        turn_left()
+        put()
+        move()
+
 
 
 
